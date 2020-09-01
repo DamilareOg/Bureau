eb793b4749c05114f34e6425202ffbce9c446b2a
DamilareOg <newagedami@gmail.com>
2020-09-01 16:29:31 +0100

    dope

--- incomeexpensesapi/settings.py
+++ incomeexpensesapi/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '@6p-h7#oy4unyb4+(@i&3eq(knbkvjkeyv&@*8+a%f45b@mfm1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["hacka-labs.herokuapp.com"]
 
 AUTH_USER_MODEL = 'authentication.User'
 # Application definition
